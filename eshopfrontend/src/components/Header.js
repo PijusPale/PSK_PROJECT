f0e0c18482110f6d4e5016394a9578d601d6b1fc
Arnanasas <arnas.bulk@gmail.com>
2023-05-18 00:20:09 +0300

    generated gitignore, initialized bootstrap, created Add-Product page and functionality, styled header

--- eshopfrontend/src/components/Header.js
+++ eshopfrontend/src/components/Header.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Header.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <h1><Link to="/">PASSATSHOP</Link></h1>
-        <ul>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/faq">FAQ</Link></li>
-          <li><Link to="/contacts">Contacts</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <Navbar bg="light" expand="lg">
+      <Container>
+      <Navbar.Brand>
+        <Link to="/">PassatShop</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/add-product">Add Product</Nav.Link>
+          <Nav.Link as={Link} to="/faq">FAQ</Nav.Link>
+          <Nav.Link as={Link} to="/contacts">Contacts</Nav.Link>
+          <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
