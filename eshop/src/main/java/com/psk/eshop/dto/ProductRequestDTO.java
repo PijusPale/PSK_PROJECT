b4e56b56a38ee59447c85f079ca7b95f28345a29
Arnanasas <arnas.bulk@gmail.com>
2023-05-17 16:57:49 +0300

    Fixed Product POST, integrated with CDN and temporary Cors fix allowing all methods

--- eshop/src/main/java/com/psk/eshop/dto/ProductRequestDTO.java
+++ eshop/src/main/java/com/psk/eshop/dto/ProductRequestDTO.java
@@ -1,7 +1,10 @@
 package com.psk.eshop.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
@@ -14,4 +17,15 @@ public class ProductRequestDTO {
     private BigDecimal price;
     private String name;
     private String description;
+
+    @JsonCreator
+    public ProductRequestDTO(@JsonProperty("userId") Long userId, @JsonProperty("discountId") Long discountId,
+                             @JsonProperty("price") BigDecimal price, @JsonProperty("name") String name,
+                             @JsonProperty("description") String description) {
+        this.userId = userId;
+        this.discountId = discountId;
+        this.price = price;
+        this.name = name;
+        this.description = description;
+    }
 }
